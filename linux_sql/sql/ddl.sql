ac19700f95a047a58eeca438187578b35165aa68
Francois Lippe <francois.lippe89@gmail.com>
2022-03-11 19:34:21 +0000

    Host script and DDL

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,27 @@
+CREATE TABLE PUBLIC.host_info
+(
+    id               SERIAL NOT NULL,
+    hostname         VARCHAR NOT NULL,
+    cpu_number       INT NOT NULL,
+    cpu_architecture VARCHAR NOT NULL,
+    cpu_model        VARCHAR NOT NULL,
+    cpu_mhz          FLOAT NOT NULL,
+    L2_cache         VARCHAR NOT NULL,
+    total_mem        VARCHAR NOT NULL,
+    timestamp        VARCHAR NOT NULL
+);
+
+--INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, L2_cache, total_mem, timestamp)
+
+CREATE TABLE PUBLIC.host_usage
+(
+    timestamp        VARCHAR NOT NULL,
+    host_id          SERIAL NOT NULL,
+    memory_free      INT NOT NULL,
+    cpu_idle         INT NOT NULL,
+    cpu_kernel       INT NOT NULL,
+    disk_io          INT NOT NULL,
+    disk_available   VARCHAR NOT NULL
+);
+
+--INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
